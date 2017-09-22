304dab7c127d6b0255a71425fb0a3505e5098e5b
Shou Takenaka <sasaminn@gmail.com>
2017-09-22 11:14:19 +0900

    Add sample js code and style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,10 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+window.onload = function(){
+    var data = document.getElementById("test");
+    if (data) {
+        data.append("test");
+    }
+}
